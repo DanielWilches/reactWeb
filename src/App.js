36704b2be0a172d4fad32985bcb15b04c06f253b
daniel <danielprietowilches@gmail.com>
2021-05-07 15:06:38 -0500

    frist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-vars */
 import './App.css';
+import bandera from './assets/bandera.png'
+import Biografia from './Biografia';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const login = (
+
+    <>
+      <div className="container">
+        <div className="container-img">
+          <img src={bandera} />
+        </div>
+        <from className="container-from ">
+          <input
+            type="text"
+            className="continer-form__Input"
+            placeholder="nombre"
+          />
+          <input
+            type="password"
+            className="continer-form__Input"
+            placeholder="Contraseña"
+          />
+
+          
+        </from>
+
+        <Router>
+          <ul className="container-btns">
+            <li >
+              <Link  to="/Biografia">ingresar</Link>
+            </li>
+            <li >
+              <Link to="/Biografia">registar usuario</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/Biografia"  ><Biografia /></Route>
+          </Switch>
+        </Router>
+      </div>
+{/*       <Biografia /> */}
+    </>
   );
+
+  return login;
 }
 
 export default App;
---
+++ src/Biografia.js
@@ -0,0 +1,26 @@
+import { div } from "prelude-ls";
+
+function Biografia() {
+    const comBio = (
+        <div className="container-biografia" >
+            <h1 className="container-biografia__titulo" >
+                Bienvenido a su biografia
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris velit urna, aliquet tempus eros in, volutpat interdum nibh. Curabitur consequat elementum justo, tempor fringilla ex volutpat non. Suspendisse potenti. Phasellus semper dui orci, suscipit rutrum neque semper quis. Etiam blandit tortor sapien, non laoreet sem ultrices et. Fusce a aliquam urna. In a mattis orci.
+
+                Integer commodo consectetur tincidunt. Sed id lobortis felis, a condimentum elit. Fusce in risus a justo molestie ultrices. Ut commodo quam vitae nisl iaculis feugiat. Nunc congue convallis ipsum, non scelerisque arcu laoreet in. Nullam tempus, lorem et vehicula dictum, quam libero tincidunt dui, aliquet interdum sapien sapien ac neque. Etiam tincidunt tempor lectus et pretium. Nunc et metus mollis, egestas neque fermentum, euismod erat. Vestibulum vitae enim congue, pretium arcu sit amet, consectetur turpis. Nunc id massa sed magna ultrices fermentum sed in tellus. Etiam semper massa vitae erat mattis, sodales feugiat mi ultrices. Proin lectus augue, malesuada eget hendrerit id, efficitur ut dolor. Ut eget scelerisque ante. Nullam massa erat, pretium vel lobortis a, pretium posuere urna. Praesent elementum nunc a diam volutpat ultrices. Donec vel leo sit amet ipsum fermentum aliquam.
+
+                Nullam laoreet pretium orci non ultricies. Cras lacinia enim sed tristique dapibus. Phasellus hendrerit orci sodales tellus posuere, sed lobortis erat rutrum. Morbi quis dui suscipit, semper arcu eget, pulvinar turpis. Maecenas ac ipsum sit amet enim tincidunt convallis. Nunc imperdiet mi eros, ut tincidunt quam tincidunt eu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque fermentum tempor elementum.
+
+                Nulla ligula dui, lacinia at consectetur ut, euismod sed orci. Vestibulum in dapibus quam. Nulla sed fringilla risus. Nulla dictum ultrices convallis. In hac habitasse platea dictumst. Nunc imperdiet euismod urna, at tincidunt lorem sodales ac. Duis accumsan molestie sollicitudin. Duis a nulla vitae augue laoreet sodales viverra a velit.
+
+                Fusce imperdiet nisi id facilisis tincidunt. Cras bibendum urna a massa iaculis rutrum. Suspendisse sit amet dui metus. Fusce convallis tortor et tellus viverra varius. In id vehicula lorem. Donec pellentesque consequat varius. Quisque ultricies sit amet nisl dictum volutpat. Aenean eu eleifend diam. Integer aliquam est et velit malesuada, tempor posuere odio porta. Nulla commodo pharetra tempor. Sed consequat vulputate nulla, et vehicula justo. Aenean non dui eget risus finibus maximus. Integer pulvinar dui sit amet ligula vehicula finibus. Sed nisi nulla, pharetra non odio vel, molestie convallis tortor.
+            </p>
+        </div>
+
+    );
+    return comBio;
+}
+
+export default Biografia;
